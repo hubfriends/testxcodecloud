05f98bbbe0ebb5031a4610696c50cd8309ab5bcc
Sviatoslav Bulgakov <sviatoslav.bulgakov@check24.de>
2024-07-30 22:46:30 +0200

    first commit

---
+++ TestXcodeCloud/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestXcodeCloud
+//
+//  Created by Slava Bulgakov on 30.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestXcodeCloud/TestXcodeCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestXcodeCloudApp.swift
+//  TestXcodeCloud
+//
+//  Created by Slava Bulgakov on 30.07.24.
+//
+
+import SwiftUI
+
+@main
+struct TestXcodeCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
